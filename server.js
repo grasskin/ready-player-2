7f436e2548e0ed0ca4badf1da142a5ba857b2e59
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2021-03-06 19:50:08 -0500

    working network

--- server.js
+++ server.js
@@ -1,19 +1,90 @@
 const express = require('express');
 const ip = require('ip');
+const http = require("http"); // http server core module
+const path = require("path");
 
-const serverport = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
+// Set process name
+process.title = "networked-aframe-server";
+
+// Setup and configure Express http server.
 const app = express();
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+// Serve the example and build the bundle in development.
+if (process.env.NODE_ENV === "development") {
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpack = require("webpack");
+  const config = require("../webpack.dev");
 
-app.get('/api/test', (req, res) => {
-    res.send('Hi this is working. If you found this email navidmx@gmail.com');
+  app.use(
+    webpackMiddleware(webpack(config), {
+      publicPath: "/dist/"
+    })
+  );
+}
+
+// Start Express http server
+const webServer = http.createServer(app);
+const io = require("socket.io")(webServer);
+
+const rooms = {};
+
+io.on("connection", socket => {
+  console.log("user connected", socket.id);
+
+  let curRoom = null;
+
+  socket.on("joinRoom", data => {
+    const { room } = data;
+
+    if (!rooms[room]) {
+      rooms[room] = {
+        name: room,
+        occupants: {},
+      };
+    }
+
+    const joinedTime = Date.now();
+    rooms[room].occupants[socket.id] = joinedTime;
+    curRoom = room;
+
+    console.log(`${socket.id} joined room ${room}`);
+    socket.join(room);
+
+    socket.emit("connectSuccess", { joinedTime });
+    const occupants = rooms[room].occupants;
+    io.in(curRoom).emit("occupantsChanged", { occupants });
+  });
+
+  socket.on("send", data => {
+    io.to(data.to).emit("send", data);
+  });
+
+  socket.on("broadcast", data => {
+    socket.to(curRoom).broadcast.emit("broadcast", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log('disconnected: ', socket.id, curRoom);
+    if (rooms[curRoom]) {
+      console.log("user disconnected", socket.id);
+
+      delete rooms[curRoom].occupants[socket.id];
+      const occupants = rooms[curRoom].occupants;
+      socket.to(curRoom).broadcast.emit("occupantsChanged", { occupants });
+
+      if (occupants == {}) {
+        console.log("everybody left room");
+        delete rooms[curRoom];
+      }
+    }
+  });
 });
 
-app.listen(serverport, () => {
+webServer.listen(port, () => {
     const ipAddress = ip.address();
-    console.log(`Network access via: ${ipAddress}:${serverport}`);
-
-    console.log('server listening on port ' + serverport);
+    console.log(`Network access via: ${ipAddress}:${port}`);
+    console.log("listening on http://localhost:" + port);
 });
